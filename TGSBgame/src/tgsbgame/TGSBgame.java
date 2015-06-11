3c6d00a2e8a41041457a71f84a5954d069db9092
Julio Casillas <julio1987sara@hotmail.com>
2015-06-11 00:52:01 -0500

    6/10/2015 - View Layer commit to repository

--- TGSBgame/src/tgsbgame/TGSBgame.java
+++ TGSBgame/src/tgsbgame/TGSBgame.java
@@ -129,5 +129,9 @@ public class TGSBgame {
         
         
     }
+
+    public static Player getLocation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
