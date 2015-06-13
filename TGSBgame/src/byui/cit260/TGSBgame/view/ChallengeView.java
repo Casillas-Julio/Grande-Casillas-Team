1d621a28c1a5651b778fcbd387b28bff90b49b78
Julio Casillas <julio1987sara@hotmail.com>
2015-06-13 14:36:10 -0500

    test 6/13/2015

---
+++ TGSBgame/src/byui/cit260/TGSBgame/view/ChallengeView.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TGSBgame.view;
+
+import byui.cit260.TGSBgame.control.ChallengesControl;
+import byui.cit260.TGSBgame.model.Map;
+
+/**
+ *
+ * @author Dragon's
+ */
+public class ChallengeView {
+    
+        public ChallengeView() {
+        
+            ChallengesControl ChallengesControl = new ChallengesControl();
+    }
+    
+    private final String doubt1 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n|                  Doubt                         |"
+            + "\n--------------------------------------------------"
+            + "\n Are you here visiting NY?                        "
+            + "\n--------------------------------------------------";
+    
+    private final String doubt2 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n Are you here for a specific reason?              "
+            + "\n--------------------------------------------------";
+    
+    private final String doubt3 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n You come accross as a good person, are you religious?"
+            + "\n--------------------------------------------------";
+    
+    private final String doubt4 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n Are you one of those people that only goes to one church?"
+            + "\n--------------------------------------------------";
+            
+    private final String doubt5 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n How do you know you are going to a good one, or even if there's such a thing?"
+            + "\n--------------------------------------------------";
+    
+    private final String doubt6 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n Are you sure you are making a smart choice?"
+            + "\n--------------------------------------------------";
+    
+    private final String doubt7 = "\n"
+            + "\n--------------------------------------------------"
+            + "\n What do you think about experiencing a different faith or philosophy that can make you happy?"
+            + "\n--------------------------------------------------";
+
+    
+    public void display(Map map) {
+        int location;
+        
+        // get location
+  //      locationControl.setLocation(map.getRowCount(), map.getColumnCount());
+  //      location = locationControl.getLocation();
+        // display description for location
+  //      if (location == 1){
+  //          System.out.println(JFK);
+        }
+  //      else if (location == 2){
+  //          System.out.println(TIMESQUARE);
+        }
+  //      else {
+  //          System.out.println("\n*** Invalid location ***");
+  //      }
+  //  }
+    //we may not need this
+  //  public String getInput() {
+  //      String input;
+   //     Scanner keyboard = new Scanner(System.in); //keyboard input screen
+
+        // prompt for the input from the player selection
+   //     System.out.println("Enter choice.");
+
+        //get the selection from the keyboard
+   //     input = keyboard.nextLine();
+   //     input = input.trim();
+    //    input = input.toUpperCase();
+
+   //     return input; // Return Name
+   // }
+    //
+//}
